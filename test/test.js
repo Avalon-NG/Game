6e82ff036204d3333bd730b687ee7dd82bf6df0b
jeno5980515 <jeno5980515@yahoo.com.tw>
2017-07-13 22:35:02 +0800

    modify testHelper
    accept with state or not, if not will give a init state
    accept testSteps be one, or a array

--- test/test.js
+++ test/test.js
@@ -54,6 +54,23 @@ const ACTION_VOTE_6_FAIL = 'ACTION_VOTE_6_FAIL';
 const ACTION_VOTE_7_FAIL = 'ACTION_VOTE_7_FAIL';
 const ACTION_DRAW_VOTES_RESULT = 'ACTION_DRAW_VOTES_RESULT';
 
+const ACTION_MISSION_1_SUCCESS = 'ACTION_MISSION_1_SUCCESS';
+const ACTION_MISSION_2_SUCCESS = 'ACTION_MISSION_2_SUCCESS';
+const ACTION_MISSION_3_SUCCESS = 'ACTION_MISSION_3_SUCCESS';
+const ACTION_MISSION_4_SUCCESS = 'ACTION_MISSION_4_SUCCESS';
+const ACTION_MISSION_5_SUCCESS = 'ACTION_MISSION_5_SUCCESS';
+const ACTION_MISSION_6_SUCCESS = 'ACTION_MISSION_6_SUCCESS';
+const ACTION_MISSION_7_SUCCESS = 'ACTION_MISSION_7_SUCCESS';
+const ACTION_MISSION_1_FAIL = 'ACTION_MISSION_1_FAIL'; 
+const ACTION_MISSION_2_FAIL = 'ACTION_MISSION_2_FAIL'; 
+const ACTION_MISSION_3_FAIL = 'ACTION_MISSION_3_FAIL'; 
+const ACTION_MISSION_4_FAIL = 'ACTION_MISSION_4_FAIL'; 
+const ACTION_MISSION_5_FAIL = 'ACTION_MISSION_5_FAIL'; 
+const ACTION_MISSION_6_FAIL = 'ACTION_MISSION_6_FAIL'; 
+const ACTION_MISSION_7_FAIL = 'ACTION_MISSION_7_FAIL'; 
+
+const ACTION_DRAW_MISSIONS_RESULT = 'ACTION_DRAW_MISSIONS_RESULT';
+
 const ACTION_MAP = {
   [ACTION_INIT_GAME] : initGame({ users : TEST_VALUE_USER_7 }),
   [ACTION_START_ROUND] : startRound(),
@@ -72,7 +89,22 @@ const ACTION_MAP = {
   [ACTION_VOTE_6_FAIL] : vote({ index : 5 , vote : -1 }),
   [ACTION_VOTE_7_SUCCESS] : vote({ index : 6 , vote : 1 }),
   [ACTION_VOTE_7_FAIL] : vote({ index : 6 , vote : -1 }),
-  [ACTION_DRAW_VOTES_RESULT] : drawVotesResult()
+  [ACTION_DRAW_VOTES_RESULT] : drawVotesResult(),
+  [ACTION_MISSION_1_SUCCESS] : executeMission({ index : 0 , mission : 1 }),
+  [ACTION_MISSION_2_SUCCESS] : executeMission({ index : 1 , mission : 1 }),
+  [ACTION_MISSION_3_SUCCESS] : executeMission({ index : 2 , mission : 1 }),
+  [ACTION_MISSION_4_SUCCESS] : executeMission({ index : 3 , mission : 1 }),
+  [ACTION_MISSION_5_SUCCESS] : executeMission({ index : 4 , mission : 1 }),
+  [ACTION_MISSION_6_SUCCESS] : executeMission({ index : 5 , mission : 1 }),
+  [ACTION_MISSION_7_SUCCESS] : executeMission({ index : 6 , mission : 1 }),
+  [ACTION_MISSION_1_FAIL] : executeMission({ index : 0 , mission : -1 }),
+  [ACTION_MISSION_2_FAIL] : executeMission({ index : 1 , mission : -1 }),
+  [ACTION_MISSION_3_FAIL] : executeMission({ index : 2 , mission : -1 }),
+  [ACTION_MISSION_4_FAIL] : executeMission({ index : 3 , mission : -1 }),
+  [ACTION_MISSION_5_FAIL] : executeMission({ index : 4 , mission : -1 }),
+  [ACTION_MISSION_6_FAIL] : executeMission({ index : 5 , mission : -1 }),
+  [ACTION_MISSION_7_FAIL] : executeMission({ index : 6 , mission : -1 }),
+  [ACTION_DRAW_MISSIONS_RESULT] : drawMissionsResult()
 }
 
 const TEST_STEPS_BEFORE_INIT = [];
@@ -121,11 +153,21 @@ const TEST_STEPS_VOTE_5_FAIL =
 
 const reducer = makeFSMReducer(STATE_MAP,ACTIONS);
 
-const testHelper = (testSteps, state) => {
-  let _state = state && Object.assign({},state) || reducer(undefined,{});
-  testSteps.forEach((el) => {
+const testHelper = (state, testSteps) => {
+  let _state, _testSteps;
+
+  if ( state !== undefined && testSteps === undefined ) {
+    _testSteps = typeof state === 'string' ? [state] : state.slice(0);
+    _state = reducer(undefined,{});
+  } else {
+    _testSteps = typeof testSteps === 'string' ? [testSteps] : testSteps.slice(0);
+    _state = Object.assign({},state);
+  }
+
+  _testSteps.forEach((el) => {
     _state = reducer(_state,ACTION_MAP[el]);
   })
+
   return _state;
 }
 
@@ -288,8 +330,8 @@ describe('basic 7 people game',()=>{
     const state = testHelper(TEST_STEPS_FIRST_BUILD_TEAM.concat(TEST_STEPS_VOTES_ALL_SUCCESS_DRAWRESULT));
     describe('do one mission',() => {      
       it('should return correct state',() => {
-        const _state = reducer(state,executeMission({ index : 1 , mission : 1 }));
-        const { status, value } = _state; 
+        const _state = testHelper(state,ACTION_MISSION_2_SUCCESS);
+        const { status, value } = _state;
         expect(status).equal(STATUS_MISSION);
         expect(value.missions[0]).equal(0);
         expect(value.missions[1]).equal(1);
@@ -298,8 +340,10 @@ describe('basic 7 people game',()=>{
     })
     describe('do all missions',() => {
       it('should return correct state',() => {
-        let _state = reducer(state,executeMission({ index : 1 , mission : 1 }));
-        _state = reducer(_state,executeMission({ index : 0 , mission : 1 }));
+        const _state = testHelper(state,[
+          ACTION_MISSION_1_SUCCESS,
+          ACTION_MISSION_2_SUCCESS
+        ]);
         const { status, value } = _state; 
         expect(status).equal(STATUS_MISSION_FINISHED);
       })
@@ -309,9 +353,11 @@ describe('basic 7 people game',()=>{
   describe('draw missions result',() => {
     const state = testHelper(TEST_STEPS_FIRST_BUILD_TEAM.concat(TEST_STEPS_VOTES_ALL_SUCCESS_DRAWRESULT));
     describe('mission success',() => {
-      let _state = reducer(state,executeMission({ index : 1 , mission : 1 }));
-      _state = reducer(_state,executeMission({ index : 0 , mission : 1 }));
-      _state = reducer(_state,drawMissionsResult());
+      const _state = testHelper(state,[
+        ACTION_MISSION_1_SUCCESS,
+        ACTION_MISSION_2_SUCCESS,
+        ACTION_DRAW_MISSIONS_RESULT
+      ]);
       it('should return correct state',() => {
         const { status, value } = _state;
         expect(status).equal(STATUS_INIT);
@@ -319,9 +365,11 @@ describe('basic 7 people game',()=>{
       })
     })
     describe('mission fail',() => {
-      let _state = reducer(state,executeMission({ index : 1 , mission : 1 }));
-      _state = reducer(_state,executeMission({ index : 0 , mission : -1 }));
-      _state = reducer(_state,drawMissionsResult());
+      const _state = testHelper(state,[
+        ACTION_MISSION_1_SUCCESS,
+        ACTION_MISSION_2_FAIL,
+        ACTION_DRAW_MISSIONS_RESULT
+      ]);
       it('should return correct state',() => {
         const { status, value } = _state;
         expect(status).equal(STATUS_INIT);
@@ -330,15 +378,15 @@ describe('basic 7 people game',()=>{
     })
   })
 
-  describe('gameover',() => {
-    const state = testHelper(TEST_STEPS_FIRST_BUILD_TEAM.concat(TEST_STEPS_VOTES_ALL_SUCCESS_DRAWRESULT));
-    describe('reach 3 missions fail',() => {
-      let _state = reducer(state,executeMission({ index : 1 , mission : 1 }));
-      _state = reducer(_state,executeMission({ index : 0 , mission : 1 }));
-      _state = reducer(_state,drawMissionsResult());
-      _state = testHelper(TEST_STEPS_VOTES_ALL_SUCCESS_DRAWRESULT,_state);
-      //console.log(_state);
-    })
-  })
+  // describe('gameover',() => {
+  //   const state = testHelper(TEST_STEPS_FIRST_BUILD_TEAM.concat(TEST_STEPS_VOTES_ALL_SUCCESS_DRAWRESULT));
+  //   describe('reach 3 missions fail',() => {
+  //     let _state = reducer(state,executeMission({ index : 1 , mission : 1 }));
+  //     _state = reducer(_state,executeMission({ index : 0 , mission : 1 }));
+  //     _state = reducer(_state,drawMissionsResult());
+  //     _state = testHelper(_state,TEST_STEPS_VOTES_ALL_SUCCESS_DRAWRESULT);
+  //     //console.log(_state);
+  //   })
+  // })
 
 })
